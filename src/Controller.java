c2951d5d66b5a6be0d2f01e8d51bb0e4c9a397d0
Francisco Castillo <cas21562@uvg.edu.gt>
2022-02-08 18:39:49 -0600

    File handeling implemented

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,8 @@
 import java.util.Vector;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 /**
  * This is the Controller of the MVC implementation for the Sorts profiling
@@ -9,10 +13,47 @@ public class Controller {
     private Vector<Student> data;
 
     /**
+     * Constructor for the Controller
+     */
+    public Controller(){
+        v = new Vista();
+        data = new Vector<>();
+        sorts = new Sorts(data);
+    }
+
+    /**
      * Driver program, this is the core of the project.
      */
     public void execute(){
+        v.start();
+        // TO CHECK IF THE FILE ALREADY EXISTS
+        if(readFile()){
+            // APPLY THE SELECTED SORT
+        }else {
+            createUnsortedFile(v.askInt(10, 3000));
+        }
+    }
 
+    /** Tries to read the file 'data.txt' on the same path.
+     * @return boolean TRUE if the file was successfully read, FALSE if not
+     */
+    private boolean readFile(){
+        try{
+            File file = new File("src\\data.txt");
+            Scanner fileReader = new Scanner(file);
+            while(fileReader.hasNextLine()){
+                String line = fileReader.nextLine();
+                String[] temp = line.split(",");
+                // CREATES A NEW STUDENT WITH THE FORMAT ON THE FILE (e.g. cas,21562)
+                data.add(new Student(temp[0], Integer.parseInt(temp[1]) ) );
+            }
+            fileReader.close();
+            v.readSuccessfully();
+            return true;
+        }catch(FileNotFoundException e){
+            v.fileNotFound();
+            return false;
+        }
     }
 
     /**
@@ -30,11 +71,29 @@ public class Controller {
      * @param n The amount of students to generate
      */
     private void createUnsortedFile(int n){
+        String[] names = {"Castillo", "Centes", "Quezada", "Cerna", "Casasola", "Reyes"}; //THE RANDOM NAMES ASSIGNED TO STUDENTS
 
+        int studentCount = 0;
+        String temp = "";
+        while(studentCount <= n){
+            temp = names[getRandomInt(0, names.length)]; //GETS A RANDOM NAME
+            temp += ",";
+            temp += getRandomInt(0,22000);
+            // TEMP FORMAT BY HERE: name,code
+            try {
+                FileWriter fileWriter = new FileWriter("src\\data.txt");
+                fileWriter.write(temp);
+                fileWriter.close();
+            }
+            catch (Exception e) {
+                e.getStackTrace();
+            }
+
+        }
     }
 
     /**
-     * Converts the sorted data from the vector to a file called 'sortedData.txt'
+     * Converts the sorted data from the vector to a file called 'data.txt'
      * Note: It must be sorted before calling this method.
      */
     private void createSortedFile(){
--- src/Vista.java
+++ src/Vista.java
@@ -1,10 +1,50 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
  * Vista implementation of the MVS design model
  */
 public class Vista {
-    private void prnt(String text){
+    private Scanner scan;
+    public Vista(){
+        scan = new Scanner(System.in);
+    }
 
+    private void prnt(String text){
+        System.out.println(text);
+    }
+    private void separator(){
+        prnt("---------------------------------------");
+    }
+    public void start(){
+        prnt("Bienvenido al programa de ordenamientos");
+        prnt("* leyendo 'data.txt' *");
+    }
+    public void readSuccessfully(){
+        prnt("Se ha leido exitosamente!");
+        separator();
+    }
+    public void fileNotFound(){
+        prnt("Actualmente no existe 'data.txt'");
+        prnt("* creando el archivo *");
+        separator();
+    }
+    public int askInt(int lowerLimit, int upperLimit){
+        separator();
+        prnt("Ingrese la cantidad de datos que desea generar:");
+        while(true){
+            try{
+                int option = scan.nextInt();
+                scan.nextLine();
+                if(option >= lowerLimit && option <= upperLimit){
+                    return option;
+                }else{
+                    throw new InputMismatchException();
+                }
+            }catch(InputMismatchException e){
+                scan.next();
+                prnt("No ha ingresado un numero valido");
+            }
+        }
     }
 }
